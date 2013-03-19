0f50eb2972aec9d9f0b142aa1e306d28df52db75
unknown <holger@holgi.amarin.home>
2013-03-19 20:04:08 +0100

    whitespace

--- Scripts/WeaponRotator-LowBudgetEquipment.js
+++ Scripts/WeaponRotator-LowBudgetEquipment.js
@@ -5,14 +5,14 @@ this.author         = "Holger Böhnke"
 this.copyright      = "(C) 2013 Holger Böhnke"
 this.licence        = "Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported"
 this.description    = "This script implements the low budget weapon rotator.";
-this.version        = "0.1"
+this.version        = "0.2"
 
 this.activated = function()
 {
-	worldScripts.WeaponRotatorMain._rotateWeapons(true);
+  worldScripts.WeaponRotatorMain._rotateWeapons(true);
 }
 
 this.mode = function()
 {
-	worldScripts.WeaponRotatorMain._rotateWeapons(false);
+  worldScripts.WeaponRotatorMain._rotateWeapons(false);
 }
