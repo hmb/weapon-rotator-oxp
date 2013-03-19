a115234e40ae7f333a208b061a1c8da5832208b8
unknown <holger@holgi.amarin.home>
2013-03-19 19:45:16 +0100

    change this.$xxx to this._xxx
    This is more like other programming languages as the '$' usually is as
    special character not usable as variable name.

--- Scripts/WeaponRotator-Main.js
+++ Scripts/WeaponRotator-Main.js
@@ -11,12 +11,12 @@ this.version        = "0.3"
 // private global properties
 
 // prefix to mission variables
-this.$storablePrefix = "weaponRotator";
+this._storablePrefix = "weaponRotator";
 
 // internal variables that should be stored in missionVariables
-this.$storables = [
-    { name: "operationTotal", defaultValue: 4711 },
-    { name: "operationCount", defaultValue: 8115 }
+this._storables = [
+    { name: "_operationTotal", defaultValue: 4711 },
+    { name: "_operationCount", defaultValue: 8115 }
   ];
 
 // --------------------------------------------
@@ -24,7 +24,7 @@ this.$storables = [
 
 this.startUp = function()
 {
-  this.$rotating = false;    // init double invocation flag
+  this._rotating = false;    // init double invocation flag
   this._loadMissionVariables();
   this._init();
 }
@@ -43,7 +43,7 @@ this.playerBoughtEquipment = function(equipmentKey)
   if (equipmentKey == "EQ_RENOVATION") {
     // reset operation counter, the total counter is
     // only reset when buying a new EQ
-    this.$operationCount = 0;
+    this._operationCount = 0;
   }
   else if (equipmentKey == "EQ_LB_WEAPON_ROTATOR") {
     // check if we have to remove the high quality WR
@@ -66,17 +66,17 @@ this.playerBoughtEquipment = function(equipmentKey)
       // refund credits according to the operation counter
       // TODO: praxis test, check if this formula is reasonable
       // TODO: account for damaged equipment
-      var countFactor = this._calcValueDiminishFactor(this.$operationCount);
-      var totalFactor = this._calcValueDiminishFactor(this.$operationTotal);
-      totalFactor = (totalFactor-1) * this.$budgetFactor + 1;
+      var countFactor = this._calcValueDiminishFactor(this._operationCount);
+      var totalFactor = this._calcValueDiminishFactor(this._operationTotal);
+      totalFactor = (totalFactor-1) * this._budgetFactor + 1;
 
       player.credits +=
         equipment2Remove.price / 10.0 / countFactor / totalFactor / damageFactor;
     }
 
     // new device: reset usage and operation counter
-    this.$operationTotal = 0;
-    this.$operationCount = 0;
+    this._operationTotal = 0;
+    this._operationCount = 0;
     this._init();
   }
 }
@@ -88,56 +88,56 @@ this._init = function()
 {
   if (this._isEquipmentPresent("EQ_LB_WEAPON_ROTATOR")) {
     // load sounds
-    this.$sndStart          = new SoundSource;
-    this.$sndStart.sound    = "weapon-rotator-lb-start.ogg";
-    this.$sndStart.loop     = false;
-    this.$sndLoop           = new SoundSource;
-    this.$sndLoop.sound     = "weapon-rotator-lb-loop.ogg";
-    this.$sndLoop.loop      = true;
-    this.$sndFinish         = new SoundSource;
-    this.$sndFinish.sound   = "weapon-rotator-lb-finish.ogg";
-    this.$sndFinish.loop    = false;
+    this._sndStart          = new SoundSource;
+    this._sndStart.sound    = "weapon-rotator-lb-start.ogg";
+    this._sndStart.loop     = false;
+    this._sndLoop           = new SoundSource;
+    this._sndLoop.sound     = "weapon-rotator-lb-loop.ogg";
+    this._sndLoop.loop      = true;
+    this._sndFinish         = new SoundSource;
+    this._sndFinish.sound   = "weapon-rotator-lb-finish.ogg";
+    this._sndFinish.loop    = false;
     // init parameters
-    this.$startLen      = 1.75;
-    this.$loopLen       = 5;
-    this.$budgetFactor  = 0.3;
-    this.$wpHq          = false;
+    this._startLen      = 1.75;
+    this._loopLen       = 5;
+    this._budgetFactor  = 0.3;
+    this._wpHq          = false;
   }
   else if (this._isEquipmentPresent("EQ_HQ_WEAPON_ROTATOR")) {
     // load sounds
-    this.$sndStart          = new SoundSource;
-    this.$sndStart.sound    = "weapon-rotator-hq-start.ogg";
-    this.$sndStart.loop     = false;
-    this.$sndLoop           = new SoundSource;
-    this.$sndLoop.sound     = "weapon-rotator-hq-loop.ogg";
-    this.$sndLoop.loop      = true;
-    this.$sndFinish         = new SoundSource;
-    this.$sndFinish.sound   = "weapon-rotator-hq-finish.ogg";
-    this.$sndFinish.loop    = false;
+    this._sndStart          = new SoundSource;
+    this._sndStart.sound    = "weapon-rotator-hq-start.ogg";
+    this._sndStart.loop     = false;
+    this._sndLoop           = new SoundSource;
+    this._sndLoop.sound     = "weapon-rotator-hq-loop.ogg";
+    this._sndLoop.loop      = true;
+    this._sndFinish         = new SoundSource;
+    this._sndFinish.sound   = "weapon-rotator-hq-finish.ogg";
+    this._sndFinish.loop    = false;
     // init parameters
-    this.$startLen      = 0.15;
-    this.$loopLen       = 1;
-    this.$budgetFactor  = 0.1;
-    this.$wpHq          = true;
+    this._startLen      = 0.15;
+    this._loopLen       = 1;
+    this._budgetFactor  = 0.1;
+    this._wpHq          = true;
   }
   else {
-    this.$sndStart      = null;
-    this.$sndLoop       = null;
-    this.$sndFinish     = null;
-    this.$startLen      = 0;
-    this.$loopLen       = 0;
-    this.$budgetFactor  = 0;
-    this.$wpHq          = false;
+    this._sndStart      = null;
+    this._sndLoop       = null;
+    this._sndFinish     = null;
+    this._startLen      = 0;
+    this._loopLen       = 0;
+    this._budgetFactor  = 0;
+    this._wpHq          = false;
   }
 }
 
 this._rotateWeapons = function(clockwise)
 {
   // avoid double invocation
-  if (this.$rotating)
+  if (this._rotating)
     return;
 
-  this.$rotating = true;
+  this._rotating = true;
 
   var maxHeat = player.ship.laserHeatLevelForward;
   maxHeat = maxHeat > player.ship.laserHeatLevelAft       ? maxHeat : player.ship.laserHeatLevelAft;
@@ -146,23 +146,23 @@ this._rotateWeapons = function(clockwise)
 
   // check temperature of hottest laser ( < 0.25 is green state)
   // the low budget version needs lower temperature to rotate
-  if (maxHeat > (this.$wpHq? 0.25 : 0.1)) {
+  if (maxHeat > (this._wpHq? 0.25 : 0.1)) {
     var msg = "Security override: Laser temperature exceeding rotation specification.";
     player.consoleMessage(msg);
-    this.$rotating = false;
+    this._rotating = false;
     return;
   }
 
   // start sound and timer
-  this.$sndStart.play();
-  this.$rotationTimer = new Timer(this, this._startLoop, this.$startLen)
+  this._sndStart.play();
+  this._rotationTimer = new Timer(this, this._startLoop, this._startLen)
 
   // remember data and weapons
-  this.$clockwise = clockwise;
-  this.$forwardWeapon = player.ship.forwardWeapon;
-  this.$portWeapon = player.ship.portWeapon;
-  this.$aftWeapon = player.ship.aftWeapon;
-  this.$starboardWeapon = player.ship.starboardWeapon;
+  this._clockwise = clockwise;
+  this._forwardWeapon = player.ship.forwardWeapon;
+  this._portWeapon = player.ship.portWeapon;
+  this._aftWeapon = player.ship.aftWeapon;
+  this._starboardWeapon = player.ship.starboardWeapon;
 
   // remove weapons, so no fire is possible
   player.ship.forwardWeapon = null;
@@ -171,7 +171,7 @@ this._rotateWeapons = function(clockwise)
   player.ship.starboardWeapon = null;
 
   // replace crosshairs with custom version, depending on rotation direction
-  this.$crosshairs = player.ship.crosshairs;
+  this._crosshairs = player.ship.crosshairs;
   if (clockwise) {
     player.ship.crosshairs = "weapon-rotator-xhairs-r.plist";
   }
@@ -179,53 +179,53 @@ this._rotateWeapons = function(clockwise)
     player.ship.crosshairs = "weapon-rotator-xhairs-l.plist";
   }
 
-  ++this.$operationTotal;
-  ++this.$operationCount;
+  ++this._operationTotal;
+  ++this._operationCount;
 }
 
 this._startLoop = function()
 {
   // stop old timer
-  this.$rotationTimer.stop();
-  delete this.$rotationTimer;
+  this._rotationTimer.stop();
+  delete this._rotationTimer;
   // setup loop timer
-  this.$rotationTimer = new Timer(this, this._finishRotation, this.$loopLen)
+  this._rotationTimer = new Timer(this, this._finishRotation, this._loopLen)
   // start looping sound
-  this.$sndStart.stop();
-  this.$sndLoop.play();
+  this._sndStart.stop();
+  this._sndLoop.play();
 }
 
 this._finishRotation = function()
 {
   // stop timer
-  this.$rotationTimer.stop();
-  delete this.$rotationTimer;
+  this._rotationTimer.stop();
+  delete this._rotationTimer;
 
-  this.$sndLoop.stop();
-  this.$sndFinish.play();
+  this._sndLoop.stop();
+  this._sndFinish.play();
 
   // re-fit rotated weapons
-  if (this.$clockwise) {
-    player.ship.forwardWeapon = this.$portWeapon;
-    player.ship.portWeapon = this.$aftWeapon;
-    player.ship.aftWeapon = this.$starboardWeapon;
-    player.ship.starboardWeapon = this.$forwardWeapon;
+  if (this._clockwise) {
+    player.ship.forwardWeapon = this._portWeapon;
+    player.ship.portWeapon = this._aftWeapon;
+    player.ship.aftWeapon = this._starboardWeapon;
+    player.ship.starboardWeapon = this._forwardWeapon;
    }
    else {
-    player.ship.forwardWeapon = this.$starboardWeapon;
-    player.ship.portWeapon = this.$forwardWeapon;
-    player.ship.aftWeapon = this.$portWeapon;
-    player.ship.starboardWeapon = this.$aftWeapon;
+    player.ship.forwardWeapon = this._starboardWeapon;
+    player.ship.portWeapon = this._forwardWeapon;
+    player.ship.aftWeapon = this._portWeapon;
+    player.ship.starboardWeapon = this._aftWeapon;
   }
 
   // forget remembered weapons
-  this.$forwardWeapon = null;
-  this.$portWeapon = null;
-  this.$aftWeapon = null;
-  this.$starboardWeapon = null;
+  this._forwardWeapon = null;
+  this._portWeapon = null;
+  this._aftWeapon = null;
+  this._starboardWeapon = null;
   // re-install crosshairs
-  player.ship.crosshairs = this.$crosshairs;
-  this.$crosshairs = null;
+  player.ship.crosshairs = this._crosshairs;
+  this._crosshairs = null;
 
   // determine viewport weapon for display message
   var viewWeapon = false;
@@ -260,7 +260,7 @@ this._finishRotation = function()
   player.consoleMessage(message);
 
   // quit rotating
-  this.$rotating = false;
+  this._rotating = false;
 };
 
 
@@ -270,27 +270,27 @@ this._finishRotation = function()
 
 this._getMissionVariable = function(varName, defaultValue)
 {
-  var missVar = missionVariables[this.$storablePrefix + "_" + varName];
-  this["$" + varName] = missVar==null? defaultValue : missVar;
+  var missVar = missionVariables[this._storablePrefix + varName];
+  this[varName] = missVar==null? defaultValue : missVar;
 }
 
 this._setMissionVariable = function(varName)
 {
-  missionVariables[this.$storablePrefix + "_" + varName] = this["$" + varName];
+  missionVariables[this._storablePrefix + varName] = this[varName];
 }
 
 this._loadMissionVariables = function()
 {
-  for (var i=0; i<this.$storables.length; ++i) {
-    var storeItem = this.$storables[i];
+  for (var i=0; i<this._storables.length; ++i) {
+    var storeItem = this._storables[i];
     this._getMissionVariable(storeItem.name, storeItem.defaultValue);
   }
 }
 
 this._saveMissionVariables = function()
 {
-  for (var i=0; i<this.$storables.length; ++i) {
-    this._setMissionVariable(this.$storables[i].name);
+  for (var i=0; i<this._storables.length; ++i) {
+    this._setMissionVariable(this._storables[i].name);
   }
 }
 
